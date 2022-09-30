742170a6c895ae0dc8c733020ab4c5cc8be23eb5
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-10-01 02:43:12 +0500

    Project Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Select from "react-select";
+import { ScheduleMeeting } from "react-schedule-meeting";
+const App = () => {
+  const availableTimeslots = [0, 1, 2, 3, 4, 5].map((id) => {
+    return {
+      id,
+      startTime: new Date(
+        new Date(new Date().setDate(new Date().getDate() + id)).setHours(
+          0,
+          0,
+          0,
+          0
+        )
+      ),
+      endTime: new Date(
+        new Date(new Date().setDate(new Date().getDate() + id)).setHours(
+          24,
+          0,
+          0,
+          0
+        )
+      ),
+    };
+  });
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "10px",
+        }}
+      >
+        <Select
+          name="Select Mentor"
+          isSearchable={true}
+          options={[
+            {
+              value: "Demo Mentor",
+              label: "Demo Mentor",
+            },
+          ]}
+        />
+      </div>
+      <ScheduleMeeting
+        borderRadius={10}
+        primaryColor="#3f5b85"
+        eventDurationInMinutes={60}
+        availableTimeslots={availableTimeslots}
+        onStartTimeSelect={console.log}
+      />
+    </>
   );
-}
+};
 
 export default App;
